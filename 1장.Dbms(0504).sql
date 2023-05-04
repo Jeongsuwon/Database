bcefc2cbf20830f1dc43a87a635da399fe89eeee
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-05-04 17:56:03 +0900

    조건절, 연산자
    산술, 비교, 연결

---
+++ 1장.Dbms(0504).sql
@@ -0,0 +1,6 @@
+--1장. DBMS
+--DataBase Management System : 데이터 관리 시스템
+--데이터 : 가공되지 않은 값, 사실(fact)
+--정보 : 데이터를 가공하거나 의사결정에 필요한 지식, 지혜
+--용어 사전은 네이버 검색 권장(oracle dbms market share)
+
--- 2.1데이터조회(0503).sql
+++ 2.1데이터조회(0503).sql
@@ -12,8 +12,29 @@ select * from departments;
 select * from job_history;
 select * from jobs;
 select * from locations;
-select * from regions
+select * from regions;
 --2. 데이터를 조회하는 select
 --select * from 테이블명;
 
 --※ 왼쪽 접속에서 각 테이블을 더블클릭하고, [데이터] 컬럼을 클릭하면 같은 기능
+
+/*
+HR 스키마에 있는 테이블(=오라클 데이터베이스 객체 중 하나)
+==========================================================
+이름                  정보
+----------------------------------------------------------
+
+*/
+
+-- 2.1 테이블 구조
+-- 오라클(=데이터베이스 관리 시스템)이 데이터를 2차원 구조(표, table의 행과 열로)로 기본적으로 저장
+-- 테이블 : 어떤 정보가 있는데 <--> 데이터베이스 : 관계(Relation)
+-- ex> (회사) 부서 정보 테이블, 사원 정보 테이블
+-- ※ Document : 행, 열 구조가 아님 vs RDBMS : 테이블(행, 열)
+
+select * from employees where department_id=80;
+
+select * from departments where department_id=80;
+
+select department_id, department_name, location_id from departments where department_id = 80;
+select department_id, department_name, location_id from departments where department_name = 'Sales'; --문자는 '로 작성, 대/소문자 구분[날짜도]
